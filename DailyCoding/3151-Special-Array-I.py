84a7423eebacb9d122e7f21ca53724fecf1b3599
Thanushanth <93480395+THANU16@users.noreply.github.com>
2025-02-01 11:59:15 +0530

    [3151] [Time Beats: 23.22%] [Memory Beats: 12.54%] - LeetPush

--- DailyCoding/3151-Special-Array-I.py
+++ DailyCoding/3151-Special-Array-I.py
@@ -1,11 +1,21 @@
+# class Solution:
+#     def isArraySpecial(self, nums: List[int]) -> bool:
+#         if len(nums) <= 1:
+#             return True
+#         for i in range(1, len(nums)):
+#             if nums[i-1] %2 == 0 and nums[i]%2 ==0:
+#                 return False
+#             elif nums[i-1] %2 == 1 and nums[i]%2 ==1:
+#                 return False
+#         return True
+
+# Method 2
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
         if len(nums) <= 1:
             return True
         for i in range(1, len(nums)):
-            if nums[i-1] %2 == 0 and nums[i]%2 ==0:
-                return False
-            elif nums[i-1] %2 == 1 and nums[i]%2 ==1:
+            if (nums[i-1] + nums[i])%2 ==0:
                 return False
         return True
 
