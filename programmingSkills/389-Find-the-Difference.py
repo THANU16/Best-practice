a9fdedabc34666ec02d180a3f5ba0979dd6abc08
Thanushanth <93480395+THANU16@users.noreply.github.com>
2025-02-02 07:09:33 +0530

    [389] [Time Beats: 100.00%] [Memory Beats: 19.39%] - LeetPush

--- programmingSkills/389-Find-the-Difference.py
+++ programmingSkills/389-Find-the-Difference.py
@@ -12,23 +12,6 @@ class Solution:
     #         tArr.remove(i)
     #     return tArr[0]
 
-    # def findTheDifference(self, s: str, t: str) -> str:
-    #     # Initialize a dictionary to store character counts
-    #     count = {}
-
-    #     # Count characters in string t
-    #     for c in t:
-    #         count[c] = count.get(c, 0) + 1
-
-    #     # Subtract counts for characters in string s
-    #     for c in s:
-    #         count[c] -= 1
-    #         if count[c] == 0:
-    #             del count[c]
-
-    #     # The remaining character in the dictionary is the difference
-    #     return list(count.keys())[0]
-
     def findTheDifference(self, s: str, t: str) -> str:
         diff = 0
         for i in t:
