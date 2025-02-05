60f4ef3a521826feec561458fa232435b953f4ed
Thanushanth <93480395+THANU16@users.noreply.github.com>
2025-02-05 12:32:26 +0530

    [242] [Time Beats: 17.03%] [Memory Beats: 57.92%] - LeetPush

---
+++ programmingSkills/242-Valid-Anagram.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashMapS = {}
+        hashMapT = {}
+
+        if len(s) != len(t):
+            return False
+        for i in range(len(s)):
+            keyS = ord(s[i])
+            keyT = ord(t[i])
+            if keyS in hashMapS.keys():
+                hashMapS[keyS] += 1
+            else:
+                hashMapS[keyS] = 1
+            
+            if keyT in hashMapT.keys():
+                hashMapT[keyT] += 1
+            else:
+                hashMapT[keyT] = 1
+
+        for i in hashMapS.keys():
+            if i in hashMapT.keys():
+                if hashMapS[i] != hashMapT[i]:
+                    return False
+            else:
+                return False
+        return True
