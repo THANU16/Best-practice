885fc0844a3964f463e05b9cb2d2e3108df4bd79
Thanushanth <93480395+THANU16@users.noreply.github.com>
2025-02-05 12:06:37 +0530

    [2326] [Time Beats: 5.44%] [Memory Beats: 6.09%] - LeetPush

---
+++ DailyCoding/2326-Spiral-Matrix-IV.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
+        matrix = []
+        direction = [(0,1), (1,0), (0,-1), (-1,0)]  #Right, Down, Left, Up
+        row , col = 0, 0
+        dirId = 0 
+        for i in range(m):
+            tempList = []
+            for j in range(n):
+                tempList.append(-1)
+            matrix.append(tempList)
+                
+        if head == None:
+            return matrix
+        node = head
+        while (node):
+            print(row, col)
+            matrix[row][col] = node.val
+            node = node.next
+            newRow = row + direction[dirId][0]
+            newCol = col + direction[dirId][1]
+
+            if not(0<= newRow < m and 0<= newCol < n and matrix[newRow][newCol] == -1):
+                dirId = (dirId + 1) %4
+                newRow = row + direction[dirId][0]
+                newCol = col + direction[dirId][1]
+            
+            row = newRow
+            col = newCol
+        return matrix
+
+
+
+
+        
