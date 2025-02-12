373c82ae2d52deffe0c01858ee2549dd973cbccc
Thanushanth <93480395+THANU16@users.noreply.github.com>
2025-02-13 01:11:44 +0530

    [43] [Time Beats: 49.27%] [Memory Beats: 45.20%] - LeetPush

---
+++ programmingSkills/43-Multiply-Strings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == \0\ or num2 == \0\:
+            return \0\
+        
+        n, m = len(num1), len(num2)
+        result = [0] * (n + m)
+
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                mul = (ord(num1[i]) - ord('0')) * (ord(num2[j]) - ord('0'))
+                pos1, pos2 = i + j, i + j + 1
+                total = mul + result[pos2]
+                
+                result[pos2] = total % 10
+                result[pos1] += total // 10
+        
+        result_str = \\.join(map(str, result)).lstrip(\0\)
+        
+        return result_str if result_str else \0\
