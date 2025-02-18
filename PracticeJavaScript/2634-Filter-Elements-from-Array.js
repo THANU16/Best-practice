3973d16bf60fd744d2e728467fd5e537137b390a
Thanushanth <93480395+THANU16@users.noreply.github.com>
2025-02-19 00:49:47 +0530

    [2634] [Time Beats: 99.83%] [Memory Beats: 7.42%] - LeetPush

---
+++ PracticeJavaScript/2634-Filter-Elements-from-Array.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    return arr.filter((value, index) => {return fn(value, index)});
+};
